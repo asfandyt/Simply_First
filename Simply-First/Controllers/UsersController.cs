66c89b0a3daf702607af8c57b828911e904dc989
gurkiratbola <guri_bola@hotmail.com>
2017-04-08 14:37:16 -0700

    generated user controller and views

--- Simply-First/Controllers/UsersController.cs
+++ Simply-First/Controllers/UsersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
@@ -10,24 +11,24 @@ using Simply_First.Models;
 
 namespace Simply_First.Controllers
 {
-    public class UserInformationsController : Controller
+    public class UsersController : Controller
     {
         private SimplyFirstVMContext db = new SimplyFirstVMContext();
 
-        // GET: UserInformations
-        public ActionResult Index()
+        // GET: Users
+        public async Task<ActionResult> Index()
         {
-            return View(db.UserInformation.ToList());
+            return View(await db.UserInformation.ToListAsync());
         }
 
-        // GET: UserInformations/Details/5
-        public ActionResult Details(string id)
+        // GET: Users/Details/5
+        public async Task<ActionResult> Details(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserInformation userInformation = db.UserInformation.Find(id);
+            UserInformation userInformation = await db.UserInformation.FindAsync(id);
             if (userInformation == null)
             {
                 return HttpNotFound();
@@ -35,37 +36,37 @@ namespace Simply_First.Controllers
             return View(userInformation);
         }
 
-        // GET: UserInformations/Create
+        // GET: Users/Create
         public ActionResult Create()
         {
             return View();
         }
 
-        // POST: UserInformations/Create
+        // POST: Users/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "UserId,FirstName,LastName,Email,StreetAddress,City,Province,PostalCode,Country,JoinDate")] UserInformation userInformation)
+        public async Task<ActionResult> Create([Bind(Include = "UserId,FirstName,LastName,Email,StreetAddress,City,Province,PostalCode,Country,JoinDate")] UserInformation userInformation)
         {
             if (ModelState.IsValid)
             {
                 db.UserInformation.Add(userInformation);
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
 
             return View(userInformation);
         }
 
-        // GET: UserInformations/Edit/5
-        public ActionResult Edit(string id)
+        // GET: Users/Edit/5
+        public async Task<ActionResult> Edit(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserInformation userInformation = db.UserInformation.Find(id);
+            UserInformation userInformation = await db.UserInformation.FindAsync(id);
             if (userInformation == null)
             {
                 return HttpNotFound();
@@ -73,30 +74,30 @@ namespace Simply_First.Controllers
             return View(userInformation);
         }
 
-        // POST: UserInformations/Edit/5
+        // POST: Users/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "UserId,FirstName,LastName,Email,StreetAddress,City,Province,PostalCode,Country,JoinDate")] UserInformation userInformation)
+        public async Task<ActionResult> Edit([Bind(Include = "UserId,FirstName,LastName,Email,StreetAddress,City,Province,PostalCode,Country,JoinDate")] UserInformation userInformation)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(userInformation).State = EntityState.Modified;
-                db.SaveChanges();
+                await db.SaveChangesAsync();
                 return RedirectToAction("Index");
             }
             return View(userInformation);
         }
 
-        // GET: UserInformations/Delete/5
-        public ActionResult Delete(string id)
+        // GET: Users/Delete/5
+        public async Task<ActionResult> Delete(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            UserInformation userInformation = db.UserInformation.Find(id);
+            UserInformation userInformation = await db.UserInformation.FindAsync(id);
             if (userInformation == null)
             {
                 return HttpNotFound();
@@ -104,14 +105,14 @@ namespace Simply_First.Controllers
             return View(userInformation);
         }
 
-        // POST: UserInformations/Delete/5
+        // POST: Users/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(string id)
+        public async Task<ActionResult> DeleteConfirmed(string id)
         {
-            UserInformation userInformation = db.UserInformation.Find(id);
+            UserInformation userInformation = await db.UserInformation.FindAsync(id);
             db.UserInformation.Remove(userInformation);
-            db.SaveChanges();
+            await db.SaveChangesAsync();
             return RedirectToAction("Index");
         }
 
