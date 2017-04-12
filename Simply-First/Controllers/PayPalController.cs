1d2fcf3b763e58e3c6a9b3e0deb98c461194175b
gurkiratbola <guri_bola@hotmail.com>
2017-04-12 14:40:58 -0700

    paypal controller

--- Simply-First/Controllers/PayPalController.cs
+++ Simply-First/Controllers/PayPalController.cs
@@ -1,4 +1,5 @@
-﻿using Simply_First.ViewModels;
+﻿using Simply_First.Services;
+using Simply_First.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,10 +12,36 @@ namespace Simply_First.Controllers
     {
         private SimplyFirstVMContext db = new SimplyFirstVMContext();
 
+        [Authorize]
         // GET: PayPal
-        public ActionResult Index()
+        public ActionResult InvoiceHistory()
         {
             return View(db.PayPal.OrderByDescending(t => t.TransactionTime));
+        }
+
+        [Authorize]
+        public ActionResult Purchase()
+        {
+            PayPalService paypalService = new PayPalService("test");
+
+            if (paypalService.TXN_ID != null)
+            {
+                PayPal paypal = new PayPal();
+                paypal.TransactionId = paypalService.TXN_ID;
+                decimal amount = Convert.ToDecimal(paypalService.PaymentGross);
+                paypal.Amount = amount;
+                paypal.BuyerEmail = paypalService.PayerEmail;
+                paypal.TransactionTime = DateTimeOffset.Now.LocalDateTime;
+                paypal.Quantity = paypalService.Quantity;
+                paypal.FirstName = paypalService.PayerFirstName;
+                paypal.LastName = paypalService.PayerLastName;
+                paypal.Custom = paypalService.Custom;
+
+                db.PayPal.Add(paypal);
+                db.SaveChanges();
+            }
+
+            return View();
         }
     }
 }
