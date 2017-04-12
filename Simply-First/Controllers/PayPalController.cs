b49e3d0482e99cb21b0aa3585a646e8bee26b8cd
gurkiratbola <guri_bola@hotmail.com>
2017-04-12 10:54:09 -0700

    added paypal controller

---
+++ Simply-First/Controllers/PayPalController.cs
@@ -0,0 +1,20 @@
+﻿using Simply_First.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Simply_First.Controllers
+{
+    public class PayPalController : Controller
+    {
+        private SimplyFirstVMContext db = new SimplyFirstVMContext();
+
+        // GET: PayPal
+        public ActionResult Index()
+        {
+            return View(db.PayPal.OrderByDescending(t => t.TransactionTime));
+        }
+    }
+}
